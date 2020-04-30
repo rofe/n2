26ea6aa88b9c476c85ea3260ac8499c2e2832606
David Nuescheler <david@nuescheler.ch>
2020-04-30 11:32:13 -0700

    Square Catalog Sync on 4/30/2020

--- filtered-catalog.js
+++ filtered-catalog.js
@@ -1945,16 +1945,6 @@ var catalog_raw=[
        "amount": 1600
       }
      }
-    },
-    {
-     "id": "2UIFLMTJWXNMEKWH3IIEXUDW",
-     "item_variation_data": {
-      "item_id": "6IKBVSZV2JOTUFVZNNQSANM4",
-      "name": "HONDURAS la peña",
-      "price_money": {
-       "amount": 1600
-      }
-     }
     }
    ]
   }
