9fcebcff61848e4e5d99ef04b83c1527f976ef35
David Nuescheler <david@nuescheler.ch>
2020-05-23 13:27:14 -0700

    Square Catalog Sync on 5/23/2020

--- filtered-catalog.js
+++ filtered-catalog.js
@@ -6154,7 +6154,7 @@ var catalog_raw=[
      "id": "5KV7PC2YOEEMOPZOCSZVXTEL",
      "item_variation_data": {
       "item_id": "BJ477UAFT44XPN5FDXKSH57Z",
-      "name": "apple cider sorbet (v)",
+      "name": "apple cider ice cream",
       "price_money": {
        "amount": 1000
       }
